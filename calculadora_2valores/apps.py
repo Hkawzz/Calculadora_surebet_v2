6ebc743cd82befdb80a19ff694ccc4fe27b484d7
Kawan <kawanhenrike6k@gmail.com>
2026-02-13 22:14:26 -0300

    inicio do projeto

---
+++ calculadora_2valores/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Calculadora2ValoresConfig(AppConfig):
+    name = 'calculadora_2valores'
---
+++ calculadora_3valores/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Calculadora3ValoresConfig(AppConfig):
+    name = 'calculadora_3valores'
