f4c0b4e75f0d2aa9fcfb7e4a4af3f743bc71783c
Kawan <kawanhenrike6k@gmail.com>
2026-02-19 22:58:12 -0300

    projeto finalizado

--- calculadora_2valores/views.py
+++ calculadora_2valores/views.py
@@ -2,25 +2,45 @@ from django.shortcuts import render
 
 def calc2(request):
     if request.method == "POST":
+        form_type = request.POST.get("form_type")
         erro = None
-        tem_surebet = "Sim"
-        try:
-            odd1 = float(request.POST.get("odd1").replace(",", "."))
-            odd2 = float(request.POST.get("odd2").replace(",", "."))
-            surebet  = (1 / odd1) + (1 / odd2)
 
-            if surebet > 1:
-                resultado = "Sem SureBet, não aposte"
-            
-            else:
-                resultado = "Tem SureBet, pode apostar"
-                tem_surebet = "Sim"
+        try:
+            if form_type == "odds":
+                #primeiro_formulario
+                odd1 = float(request.POST.get("odd1").replace(",", "."))
+                odd2 = float(request.POST.get("odd2").replace(",", "."))
+                numero1 = 1 / odd1
+                numero2 = 1 / odd2
+                surebet  = numero1 + numero2
+
+                if surebet > 1:
+                    resultado = "Sem SureBet, não aposte"
+                    tem_surebet = None
+                
+                else:
+                    resultado = "Tem SureBet, pode apostar"
+                    tem_surebet = "Sim"
+
+                return render(request, 'calculadora_2valores/index.html', {"resultado": resultado, "tem_surebet": tem_surebet, "odd1":odd1, "odd2":odd2})
+
+            elif form_type == "valor":
+                #segundo_formulario
+                odd1 = float(request.POST.get("odd1").replace(",", "."))
+                odd2 = float(request.POST.get("odd2").replace(",", "."))
+                valor_total = float(request.POST.get("valor_total").replace(",", "."))
+
+                numero1 = (1 / odd1)
+                numero2 = (1 / odd2)
+                surebet = numero1 + numero2
+                apostar1 = round((numero1 / surebet) * valor_total, 2)
+                apostar2 = round((numero2 / surebet) * valor_total, 2)
+
+                return render(request, 'calculadora_2valores/index.html', {"apostar1": apostar1, "apostar2": apostar2})
 
         except:
             erro = "ERRO: Insira números válidos"
-            resultado = None
-            tem_surebet = None
         
-        return render(request, 'calculadora_2valores/index.html', {"resultado": resultado, "erro": erro, "tem_surebet":tem_surebet})
-    
+        return render(request, 'calculadora_2valores/index.html', {"erro": erro})
+
     return render(request, "calculadora_2valores/index.html")
--- calculadora_3valores/views.py
+++ calculadora_3valores/views.py
@@ -2,23 +2,50 @@ from django.shortcuts import render
 
 def calc3(request):
     if request.method == "POST":
+        form_type = request.POST.get("form_type")
         erro = None
-        try:
-            odd1 = float(request.POST.get("odd1").replace(",", "."))
-            odd2 = float(request.POST.get("odd2").replace(",", "."))
-            odd3 = float(request.POST.get("odd3").replace(",", "."))
-            surebet  = (1 / odd1) + (1 / odd2) + (1 / odd3)
 
-            if surebet < 1:
-                resultado = "Sem SureBet, não aposte"
-            
-            else:
-                resultado = "Tem SureBet, pode apostar"
+        try:
+            if form_type == "odds":
+                #primeiro_formulario
+                odd1 = float(request.POST.get("odd1").replace(",", "."))
+                odd2 = float(request.POST.get("odd2").replace(",", "."))
+                odd3 = float(request.POST.get("odd3").replace(",", "."))
+                numero1 = 1 / odd1
+                numero2 = 1 / odd2
+                numero3 = 1 / odd3
+                surebet  = numero1 + numero2 +numero3
+
+                if surebet > 1:
+                    resultado = "Sem SureBet, não aposte"
+                    tem_surebet = None
+                
+                else:
+                    resultado = "Tem SureBet, pode apostar"
+                    tem_surebet = "Sim"
+
+                return render(request, 'calculadora_3valores/index.html', {"resultado": resultado, "tem_surebet": tem_surebet, "odd1":odd1, "odd2":odd2, "odd3":odd3})
+
+            elif form_type == "valor":
+                #segundo_formulario
+                odd1 = float(request.POST.get("odd1").replace(",", "."))
+                odd2 = float(request.POST.get("odd2").replace(",", "."))
+                odd3 = float(request.POST.get("odd3").replace(",", "."))
+                valor_total = float(request.POST.get("valor_total").replace(",", "."))
+
+                numero1 = (1 / odd1)
+                numero2 = (1 / odd2)
+                numero3 = (1 / odd3)
+                surebet = numero1 + numero2 + numero3
+                apostar1 = round((numero1 / surebet) * valor_total, 2)
+                apostar2 = round((numero2 / surebet) * valor_total, 2)
+                apostar3 = round((numero3 / surebet) * valor_total, 2)
+
+                return render(request, 'calculadora_3valores/index.html', {"apostar1": apostar1, "apostar2": apostar2, "apostar3": apostar3})
 
         except:
             erro = "ERRO: Insira números válidos"
-            resultado = None
         
-        return render(request, 'calculadora_3valores/index.html', {"resultado": resultado, "erro": erro })
-    
+        return render(request, 'calculadora_3valores/index.html', {"erro": erro})
+
     return render(request, "calculadora_3valores/index.html")
